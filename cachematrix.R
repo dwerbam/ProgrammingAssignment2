1bff8b9bcb0e442341407b713ebe60b0ea7c2e8a
Diego Werba <dwerbam@gmail.com>
2019-06-01 22:57:24 +0200

    created makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This functions are encapsulation of matrix for caching in 
+## memory some heavy processing functions
 
-## Write a short comment describing this function
-
+## This extends matrix implementation to add cache layer when 
+## calculating the matrix inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    s <- NULL
+    set <- function(y) {
+        x <<- y
+        s <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) s <<- solve
+    getsolve <- function() s
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
+## calculates the inverse of a cacheMatrix 
+## (and uses the pre-calculated cache if possible)
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    s <- x$getsolve()
+    if(!is.null(s)) {
+        message("getting cached data")
+        return(s)
+    }
+    data <- x$get()
+    s <- solve(data, ...)
+    x$setsolve(s)
+    s
+    ## Return a matrix that is the inverse of 'x'
 }
+
+## test code -- simple det!=0 matrix
+# A <- makeCacheMatrix(
+#     matrix( c(5, 1, 0,
+#               3,-1, 2,
+#               4, 0,-1), nrow=3, byrow=TRUE))
+# cacheSolve(A)
+# cacheSolve(A) # this time uses cache
+
+## test code -- creates a square matrix much bigger to teest performance gain
+# matrixSize <- 1000
+# t <- makeCacheMatrix(
+#     matrix( rnorm(matrixSize*matrixSize, mean=1), nrow=matrixSize, byrow=TRUE))
+# head(cacheSolve(t),1)
+# head(cacheSolve(t),1)  # this time uses cache
+
+
